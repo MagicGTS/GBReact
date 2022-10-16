54eb30fc9165cf1db00064d4ad99a9a7db99d7d1
root <root@kubernetes.aristo-aps.ru>
2022-10-16 15:59:51 +0300

    final

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState, useEffect ,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Routes, Route, Link, Outlet } from "react-router-dom";
 import Home from "./Home/Home.js";
 import Profile from "./Profile/Profile.js";
 import Chats from "./Chats/Chats.js";
 import ChatList from "./ChatList/ChatList.js";
-import {ThemeContext,themes} from "./Theme";
+import GistsList from "./GistsList/GistsList.js";
+import { ThemeContext, themes } from "./Theme";
 //import Grid from '@mui/material/Grid'; // Grid version 1
 
 function App() {
-  //debugger;
-
-  
-
-  //debugger;
   return (
     <div className="App">
       <ThemeContext.Provider value={themes.dark}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="chats" element={<ChatList />}>
-            <Route path=":id" element={<Chats />} />
-            <Route path=":id/:author" element={<Chats />} />
-          </Route>
-
-          {/* Using path="*"" means "match anything", so this route
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="chats" element={<ChatList />}>
+              <Route path=":id" element={<Chats />} />
+              <Route path=":id/:author" element={<Chats />} />
+            </Route>
+            <Route path="gistslist" element={<GistsList />} />
+              {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
+              <Route path="*" element={<NoMatch />} />
+            </Route>
+        </Routes>
       </ThemeContext.Provider>
     </div>
   );
@@ -48,6 +44,9 @@ function Layout() {
         </li>
         <li>
           <Link to="/chats">chats</Link>
+        </li>
+        <li>
+          <Link to="/gistslist">gists</Link>
         </li>
         <li>
           <Link to="/">Home</Link>
